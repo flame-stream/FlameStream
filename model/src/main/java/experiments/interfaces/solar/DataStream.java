53e38675b79351f4bfb93a0f967630d48d83e2e0
solar <ikuralenok@gmail.com>
2016-10-22 20:31:54 +0300

    Initial interfaces version

---
+++ model/src/main/java/experiments/interfaces/solar/DataStream.java
@@ -0,0 +1,31 @@
+package experiments.interfaces.solar;
+
+import experiments.interfaces.solar.impl.MergeStream;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+/**
+ * Experts League
+ * Created by solar on 17.10.16.
+ */
+public interface DataStream extends Stream<DataItem> {
+  Type types();
+
+  boolean isValid();
+  double alpha();
+
+  Set<Condition> violated();
+
+  static DataStream merge(DataStream... streams) {
+    return new MergeStream(streams);
+  }
+  static DataStream group(DataStream input, DataItem.Grouping hash) {
+    return null;
+  }
+
+  interface Type {
+    String name();
+    Set<Class<? extends Condition>> conditions();
+  }
+}
---
+++ model/src/main/java/experiments/interfaces/solar/StateDataStream.java
@@ -0,0 +1,8 @@
+package experiments.interfaces.solar;
+
+/**
+ * Experts League
+ * Created by solar on 17.10.16.
+ */
+public interface StateDataStream {
+}
