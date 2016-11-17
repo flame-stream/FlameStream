93896a1a5fde9f7d8db8dedb980ca1296866607f
solar <ikuralenok@gmail.com>
2016-11-17 18:30:40 +0300

    Serialization mechanisms fixed

--- model/src/main/java/com/spbsu/datastream/example/bl/UserContainer.java
+++ model/src/main/java/com/spbsu/datastream/example/bl/UserContainer.java
@@ -3,14 +3,16 @@ package com.spbsu.datastream.example.bl;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.spbsu.datastream.example.bl.counter.UserCounter;
+//import com.spbsu.datastream.example.bl.sql.UserSelector;
 
 /**
  * Experts League
  * Created by solar on 05.11.16.
  */
-@JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include= JsonTypeInfo.As.WRAPPER_OBJECT, property="type")
+@JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include= JsonTypeInfo.As.WRAPPER_OBJECT)
 @JsonSubTypes({
     @JsonSubTypes.Type(value=UserCounter.class, name="stat"),
+//    @JsonSubTypes.Type(value= UserSelector.class, name="selector"),
     @JsonSubTypes.Type(value=UserQuery.class, name="log")
 })
 public interface UserContainer {
