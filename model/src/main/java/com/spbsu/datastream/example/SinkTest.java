a8e5700f1b5d3211711e20989223ae8e9cb6a3fb
NukaCat <sqrt19@gmail.com>
2016-12-03 22:08:38 +0300

    Run Sinks

---
+++ model/src/main/java/com/spbsu/datastream/example/SinkTest.java
@@ -0,0 +1,54 @@
+package com.spbsu.datastream.example;
+
+import akka.actor.Actor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import com.spbsu.akka.ActorContainer;
+import com.spbsu.commons.system.RuntimeUtils;
+import com.spbsu.datastream.core.*;
+import com.spbsu.datastream.core.inference.DataTypeCollection;
+import com.spbsu.datastream.core.inference.TypeCollection;
+import com.spbsu.datastream.core.io.Output;
+import com.spbsu.datastream.core.job.*;
+import com.spbsu.datastream.core.job.control.EndOfTick;
+import com.spbsu.datastream.example.bl.UserGrouping;
+import com.spbsu.datastream.example.bl.counter.CountUserEntries;
+import com.spbsu.datastream.example.bl.counter.UserCounter;
+import org.apache.commons.math3.analysis.function.Sin;
+
+import java.util.function.Function;
+
+
+public class SinkTest {
+  public static void main(String[] args) {
+    final DataTypeCollection types = (DataTypeCollection) DataStreamsContext.typeCollection;
+    final ActorSystem akka = ActorSystem.create();
+    final int maxUserCount = 5000;
+
+    DataStreamsContext.input.stream(/*"ypes.type("UsersLog")*/ null).flatMap((input) -> {
+      StreamSink sink = new StreamSink();
+      //joba = types.<Integer>convert(types.type("UsersLog"), types.type("Group(UserLog, 2)"), sink);
+      Sink joba = makeJoba(akka, sink, types);
+      new Thread(() -> {
+        input.forEach((di) -> {
+          joba.accept(di);
+          //Thread.yield();
+        });
+        joba.accept(new EndOfTick());
+      }).start();
+      return sink.stream().onClose(() -> Output.instance().commit());
+    }).forEach(Output.instance().printer());
+  }
+
+  public static Sink makeJoba(ActorSystem actorSystem, Sink sink, DataTypeCollection types) {
+    final ReplicatorJoba replicator = new ReplicatorJoba(sink);
+    final FilterJoba states = new FilterJoba(replicator, null, new CountUserEntries(), RuntimeUtils.findTypeParameters(CountUserEntries.class, Function.class)[0], UserCounter.class);
+    final GroupingJoba grouping = new GroupingJoba(states, types.forName("Group(Merge(UsersLog, States), UserHash, 2)"), new UserGrouping(), 2);
+    ActorRef mergeActor = actorSystem.actorOf(ActorContainer.props(MergeActor.class, grouping, 2));
+    ActorSink mergeSink = new ActorSink(mergeActor);
+    replicator.add(mergeSink);
+    return mergeSink;
+  }
+
+}
