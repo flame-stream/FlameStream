592bf0624b71637cbee487b0e8c4a5e957ca9712
Nikita Marshalkin <marnikitta@gmail.com>
2017-04-21 12:57:29 +0300

    Simple grouping logic tests

---
+++ new-core/src/test/java/com/spbsu/datastream/core/FakeAtomicHandle.java
@@ -0,0 +1,36 @@
+package com.spbsu.datastream.core;
+
+import com.spbsu.datastream.core.graph.OutPort;
+import com.spbsu.datastream.core.graph.ops.GroupingState;
+import com.spbsu.datastream.core.tick.atomic.AtomicHandle;
+
+import java.util.function.BiConsumer;
+
+public final class FakeAtomicHandle implements AtomicHandle {
+  private final BiConsumer<OutPort, DataItem<?>> pushConsumer;
+
+  public FakeAtomicHandle(final BiConsumer<OutPort, DataItem<?>> pushConsumer) {
+    this.pushConsumer = pushConsumer;
+  }
+
+  @Override
+  public void push(final OutPort out, final DataItem<?> result) {
+    this.pushConsumer.accept(out, result);
+  }
+
+  @Override
+  public GroupingState<?> loadGroupingState() {
+    // TODO: 4/21/17 return empty state
+    throw new UnsupportedOperationException("Loading of the grouping state has not implemented yet");
+  }
+
+  @Override
+  public void saveGroupingState(final GroupingState<?> storage) {
+    throw new UnsupportedOperationException("Saving of the grouping state has not implemented yet");
+  }
+
+  @Override
+  public HashRange localRange() {
+    return new HashRange(Integer.MIN_VALUE, Integer.MAX_VALUE);
+  }
+}
---
+++ new-core/src/test/java/com/spbsu/datastream/core/graph/ops/GroupingTest.java
@@ -0,0 +1,210 @@
+package com.spbsu.datastream.core.graph.ops;
+
+import com.spbsu.datastream.core.*;
+import com.spbsu.datastream.core.tick.atomic.AtomicHandle;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class GroupingTest {
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void withoutReordering() {
+    final Grouping<String> grouping = new Grouping<>(HashFunction.constantHash(1), 2);
+
+    final List<DataItem<GroupingResult<String>>> out = new ArrayList<>();
+
+    final AtomicHandle handle = new FakeAtomicHandle((port, di) -> {
+      if (!port.equals(grouping.ackPort())) {
+        out.add((DataItem<GroupingResult<String>>) di);
+      }
+    });
+
+
+    final DataItem<String> x1 = new PayloadDataItem<>(new Meta(new GlobalTime(1, 1)), "v1");
+    final DataItem<String> x2 = new PayloadDataItem<>(new Meta(new GlobalTime(2, 1)), "v2");
+    final DataItem<String> x3 = new PayloadDataItem<>(new Meta(new GlobalTime(3, 1)), "v3");
+
+    grouping.onStart(handle);
+    grouping.onPush(grouping.inPort(), x1, handle);
+    grouping.onPush(grouping.inPort(), x2, handle);
+    grouping.onPush(grouping.inPort(), x3, handle);
+
+    final List<GroupingResult<String>> expectedResult = new ArrayList<>();
+
+    final GroupingResult<String> y1 = new GroupingResult<>(Collections.singletonList(x1.payload()), 1);
+    final GroupingResult<String> y2 = new GroupingResult<>(Arrays.asList(x1.payload(), x2.payload()), 1);
+    final GroupingResult<String> y3 = new GroupingResult<>(Arrays.asList(x2.payload(), x3.payload()), 1);
+
+    expectedResult.add(y1);
+    expectedResult.add(y2);
+    expectedResult.add(y3);
+
+    Assert.assertEquals(out.stream().map(DataItem::payload).collect(Collectors.toList()), expectedResult);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void headReordering() {
+    final Grouping<String> grouping = new Grouping<>(HashFunction.constantHash(1), 2);
+
+    final List<DataItem<GroupingResult<String>>> out = new ArrayList<>();
+
+    final AtomicHandle handle = new FakeAtomicHandle((port, di) -> {
+      if (!port.equals(grouping.ackPort())) {
+        out.add((DataItem<GroupingResult<String>>) di);
+      }
+    });
+
+    final DataItem<String> x2 = new PayloadDataItem<>(new Meta(new GlobalTime(2, 1)), "v2");
+    final DataItem<String> x1 = new PayloadDataItem<>(new Meta(new GlobalTime(1, 1)), "v1");
+    final DataItem<String> x3 = new PayloadDataItem<>(new Meta(new GlobalTime(3, 1)), "v3");
+
+    grouping.onStart(handle);
+    grouping.onPush(grouping.inPort(), x1, handle);
+    grouping.onPush(grouping.inPort(), x2, handle);
+    grouping.onPush(grouping.inPort(), x3, handle);
+
+    final List<GroupingResult<String>> expectedResult = new ArrayList<>();
+
+    final GroupingResult<String> y1 = new GroupingResult<>(Collections.singletonList(x2.payload()), 1);
+    final GroupingResult<String> y2 = new GroupingResult<>(Collections.singletonList(x1.payload()), 1);
+    final GroupingResult<String> y3 = new GroupingResult<>(Arrays.asList(x1.payload(), x2.payload()), 1);
+    final GroupingResult<String> y4 = new GroupingResult<>(Arrays.asList(x2.payload(), x3.payload()), 1);
+
+    expectedResult.add(y1);
+    expectedResult.add(y2);
+    expectedResult.add(y3);
+    expectedResult.add(y4);
+
+    Assert.assertEquals(out.stream().map(DataItem::payload).collect(Collectors.toList()), expectedResult);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void tailReordering() {
+    final Grouping<String> grouping = new Grouping<>(HashFunction.constantHash(1), 2);
+
+    final List<DataItem<GroupingResult<String>>> out = new ArrayList<>();
+
+    final AtomicHandle handle = new FakeAtomicHandle((port, di) -> {
+      if (!port.equals(grouping.ackPort())) {
+        out.add((DataItem<GroupingResult<String>>) di);
+      }
+    });
+
+    final DataItem<String> x1 = new PayloadDataItem<>(new Meta(new GlobalTime(1, 1)), "v1");
+    final DataItem<String> x3 = new PayloadDataItem<>(new Meta(new GlobalTime(3, 1)), "v3");
+    final DataItem<String> x2 = new PayloadDataItem<>(new Meta(new GlobalTime(2, 1)), "v2");
+
+    grouping.onStart(handle);
+    grouping.onPush(grouping.inPort(), x1, handle);
+    grouping.onPush(grouping.inPort(), x3, handle);
+    grouping.onPush(grouping.inPort(), x2, handle);
+
+    final List<GroupingResult<String>> expectedResult = new ArrayList<>();
+
+    final GroupingResult<String> y1 = new GroupingResult<>(Collections.singletonList(x1.payload()), 1);
+    final GroupingResult<String> y2 = new GroupingResult<>(Arrays.asList(x1.payload(), x3.payload()), 1);
+    final GroupingResult<String> y3 = new GroupingResult<>(Arrays.asList(x1.payload(), x2.payload()), 1);
+    final GroupingResult<String> y4 = new GroupingResult<>(Arrays.asList(x2.payload(), x3.payload()), 1);
+
+    expectedResult.add(y1);
+    expectedResult.add(y2);
+    expectedResult.add(y3);
+    expectedResult.add(y4);
+
+    Assert.assertEquals(out.stream().map(DataItem::payload).collect(Collectors.toList()), expectedResult);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void reverseReordering() {
+    final Grouping<String> grouping = new Grouping<>(HashFunction.constantHash(1), 2);
+
+    final List<DataItem<GroupingResult<String>>> out = new ArrayList<>();
+
+    final AtomicHandle handle = new FakeAtomicHandle((port, di) -> {
+      if (!port.equals(grouping.ackPort())) {
+        out.add((DataItem<GroupingResult<String>>) di);
+      }
+    });
+
+
+    final DataItem<String> x1 = new PayloadDataItem<>(new Meta(new GlobalTime(1, 1)), "v1");
+    final DataItem<String> x2 = new PayloadDataItem<>(new Meta(new GlobalTime(2, 1)), "v2");
+    final DataItem<String> x3 = new PayloadDataItem<>(new Meta(new GlobalTime(3, 1)), "v3");
+
+    grouping.onStart(handle);
+    grouping.onPush(grouping.inPort(), x3, handle);
+    grouping.onPush(grouping.inPort(), x2, handle);
+    grouping.onPush(grouping.inPort(), x1, handle);
+
+    final List<GroupingResult<String>> expectedResult = new ArrayList<>();
+
+    final GroupingResult<String> y1 = new GroupingResult<>(Collections.singletonList(x3.payload()), 1);
+    final GroupingResult<String> y2 = new GroupingResult<>(Collections.singletonList(x2.payload()), 1);
+    final GroupingResult<String> y3 = new GroupingResult<>(Arrays.asList(x2.payload(), x3.payload()), 1);
+    final GroupingResult<String> y4 = new GroupingResult<>(Collections.singletonList(x1.payload()), 1);
+    final GroupingResult<String> y5 = new GroupingResult<>(Arrays.asList(x1.payload(), x2.payload()), 1);
+    final GroupingResult<String> y6 = new GroupingResult<>(Arrays.asList(x2.payload(), x3.payload()), 1);
+
+    expectedResult.add(y1);
+    expectedResult.add(y2);
+    expectedResult.add(y3);
+    expectedResult.add(y4);
+    expectedResult.add(y5);
+    expectedResult.add(y6);
+
+    Assert.assertEquals(out.stream().map(DataItem::payload).collect(Collectors.toList()), expectedResult);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void reorderingWithInvalidating() {
+    final Grouping<String> grouping = new Grouping<>(HashFunction.constantHash(1), 2);
+
+    final List<DataItem<GroupingResult<String>>> out = new ArrayList<>();
+
+    final AtomicHandle handle = new FakeAtomicHandle((port, di) -> {
+      if (!port.equals(grouping.ackPort())) {
+        out.add((DataItem<GroupingResult<String>>) di);
+      }
+    });
+
+    final DataItem<String> x1 = new PayloadDataItem<>(new Meta(new GlobalTime(1, 1)), "v1");
+    final DataItem<String> x2 = new PayloadDataItem<>(new Meta(new GlobalTime(2, 1)), "v2");
+
+    final Meta x3Meta = new Meta(new GlobalTime(3, 1));
+    final DataItem<String> x3 = new PayloadDataItem<>(new Meta(x3Meta, 1), "v3");
+    final DataItem<String> x3Prime = new PayloadDataItem<>(new Meta(x3Meta, 2), "v3Prime");
+
+    grouping.onStart(handle);
+    grouping.onPush(grouping.inPort(), x1, handle);
+    grouping.onPush(grouping.inPort(), x2, handle);
+    grouping.onPush(grouping.inPort(), x3, handle);
+    grouping.onPush(grouping.inPort(), x3Prime, handle);
+
+    final List<GroupingResult<String>> expectedResult = new ArrayList<>();
+
+    final GroupingResult<String> y1 = new GroupingResult<>(Collections.singletonList(x1.payload()), 1);
+    final GroupingResult<String> y2 = new GroupingResult<>(Arrays.asList(x1.payload(), x2.payload()), 1);
+    final GroupingResult<String> y3 = new GroupingResult<>(Arrays.asList(x2.payload(), x3.payload()), 1);
+    final GroupingResult<String> y4 = new GroupingResult<>(Arrays.asList(x2.payload(), x3Prime.payload()), 1);
+    final GroupingResult<String> y5 = new GroupingResult<>(Arrays.asList(x3Prime.payload(), x3.payload()), 1);
+
+    expectedResult.add(y1);
+    expectedResult.add(y2);
+    expectedResult.add(y3);
+    expectedResult.add(y4);
+    expectedResult.add(y5);
+
+    Assert.assertEquals(out.stream().map(DataItem::payload).collect(Collectors.toList()), expectedResult);
+  }
+}
