474547095a87cb639820d5812d652274be573028
Nikita Marshalkin <marnikitta@gmail.com>
2017-05-05 15:00:49 +0300

    more hashchanging filters added

--- new-core/src/test/java/com/spbsu/datastream/core/IdentityTest.java
+++ new-core/src/test/java/com/spbsu/datastream/core/IdentityTest.java
@@ -26,15 +26,21 @@ public final class IdentityTest extends DataStreamsSuite {
   public void emptyTest() throws InterruptedException {
     final Queue<Integer> result = new ArrayDeque<>();
 
-    final StatelessFilter<Integer, Integer> filter = new StatelessFilter<>(new HumbleFiler(), HashFunction.OBJECT_HASH);
+    final StatelessFilter<Integer, Integer> filter1 = new StatelessFilter<>(new HumbleFiler(-1), HashFunction.OBJECT_HASH);
+    final StatelessFilter<Integer, Integer> filter2 = new StatelessFilter<>(new HumbleFiler(-2), HashFunction.OBJECT_HASH);
+    final StatelessFilter<Integer, Integer> filter3 = new StatelessFilter<>(new HumbleFiler(-3), HashFunction.OBJECT_HASH);
+    final StatelessFilter<Integer, Integer> filter4 = new StatelessFilter<>(new HumbleFiler(-4), HashFunction.OBJECT_HASH);
     final PreSinkMetaFilter<Integer> metaFilter = new PreSinkMetaFilter<>(HashFunction.OBJECT_HASH);
-    final ConsumerBarrierSink<Integer> println = new ConsumerBarrierSink<>(this.wrap(result));
+    final ConsumerBarrierSink<Integer> consumer = new ConsumerBarrierSink<>(this.wrap(result));
 
-    final Graph graph = filter.fuse(metaFilter, filter.outPort(), metaFilter.inPort())
-            .fuse(println, metaFilter.outPort(), println.inPort());
+    final Graph graph = filter1.fuse(filter2, filter1.outPort(), filter2.inPort())
+            .fuse(filter3, filter2.outPort(), filter3.inPort())
+            .fuse(filter4, filter3.outPort(), filter4.inPort())
+            .fuse(metaFilter, filter4.outPort(), metaFilter.inPort())
+            .fuse(consumer, metaFilter.outPort(), consumer.inPort());
 
     final Set<Integer> fronts = this.fronts();
-    final Map<Integer, InPort> frontBindings = fronts.stream().collect(Collectors.toMap(Function.identity(), e -> filter.inPort()));
+    final Map<Integer, InPort> frontBindings = fronts.stream().collect(Collectors.toMap(Function.identity(), e -> filter1.inPort()));
     final TheGraph theGraph = new TheGraph(graph, frontBindings);
 
     this.deploy(theGraph);
@@ -43,16 +49,21 @@ public final class IdentityTest extends DataStreamsSuite {
     final List<Integer> source = new Random().ints(10000).boxed().collect(Collectors.toList());
     source.forEach(this.randomConsumer());
 
-    TimeUnit.SECONDS.sleep(20);
+    TimeUnit.SECONDS.sleep(30);
 
-    Assert.assertEquals(new HashSet<>(result), source.stream().map(str -> str * 3).collect(Collectors.toSet()));
+    Assert.assertEquals(new HashSet<>(result), source.stream().map(str -> str * -1 * -2 * -3 * -4).collect(Collectors.toSet()));
   }
 
   public static final class HumbleFiler implements Function<Integer, Integer> {
+    private final int factor;
+
+    public HumbleFiler(final int factor) {
+      this.factor = factor;
+    }
 
     @Override
     public Integer apply(final Integer s) {
-      return s * 3;
+      return s * factor;
     }
   }
 }
