c944d05640e72ec1c00d52a62f1cd6860b9319c6
Artem Trofimov <trofimov9artem@gmail.com>
2017-08-21 13:01:43 +0300

    Testdirs removing fixed

--- core/src/test/java/com/spbsu/datastream/core/LocalCluster.java
+++ core/src/test/java/com/spbsu/datastream/core/LocalCluster.java
@@ -50,14 +50,14 @@ public final class LocalCluster implements Cluster {
 
   @Override
   public void close() throws Exception {
-    FileUtils.deleteDirectory(new File("zookeeper"));
-    FileUtils.deleteDirectory(new File("leveldb"));
-
     this.workerApplication.forEach(WorkerApplication::shutdown);
     this.workerApplication.clear();
 
     this.zk.shutdown();
     this.zkThread.join();
+
+    FileUtils.deleteDirectory(new File("zookeeper"));
+    FileUtils.deleteDirectory(new File("leveldb"));
   }
 
   public LocalCluster(int workersCount, int frontCount) {
