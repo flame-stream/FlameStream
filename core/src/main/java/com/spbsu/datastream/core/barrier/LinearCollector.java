f30334233e98281dc6f6edc8ef3378548ba4b9fc
Artem Trofimov <trofimov9artem@gmail.com>
2017-09-15 19:26:54 +0300

    Tiny linear collector optimization

--- core/src/main/java/com/spbsu/datastream/core/barrier/LinearCollector.java
+++ core/src/main/java/com/spbsu/datastream/core/barrier/LinearCollector.java
@@ -4,12 +4,7 @@ import com.spbsu.datastream.core.DataItem;
 import com.spbsu.datastream.core.GlobalTime;
 import com.spbsu.datastream.core.graph.ops.Grouping;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.function.Consumer;
 
 public final class LinearCollector implements BarrierCollector {
@@ -28,12 +23,12 @@ public final class LinearCollector implements BarrierCollector {
   public void enqueue(DataItem<?> item) {
     //noinspection unchecked
     final DataItem<Object> dataItem = (DataItem<Object>) item;
-    this.invalidationPool.putIfAbsent(item.meta().globalTime(), new ArrayList<>());
-
-    this.invalidationPool.computeIfPresent(item.meta().globalTime(), (key, oldList) -> {
-      // TODO: 15.09.2017 refactor this shit
-      Grouping.insert(oldList, dataItem);
-      return oldList;
+    this.invalidationPool.compute(item.meta().globalTime(), (globalTime, dataItems) -> {
+      if (dataItems == null) {
+        dataItems = new ArrayList<>();
+      }
+      Grouping.insert(dataItems, dataItem);
+      return dataItems;
       /*oldList.removeIf(di -> di.meta().trace().isInvalidatedBy(item.meta().trace()));
       if (oldList.stream().noneMatch(di -> item.meta().trace().isInvalidatedBy(di.meta().trace()))) {
         oldList.add(item);
