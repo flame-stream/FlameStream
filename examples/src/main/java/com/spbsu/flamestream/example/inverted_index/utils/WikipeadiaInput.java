c9bf838f07d504007c591ef96fd0175901036666
Nikita Marshalkin <marnikitta@gmail.com>
2017-10-06 15:45:07 +0300

    Add wikipedia to benchmark resources

--- examples/src/main/java/com/spbsu/flamestream/example/inverted_index/utils/WikipeadiaInput.java
+++ examples/src/main/java/com/spbsu/flamestream/example/inverted_index/utils/WikipeadiaInput.java
@@ -2,11 +2,7 @@ package com.spbsu.flamestream.example.inverted_index.utils;
 
 import com.spbsu.flamestream.example.inverted_index.model.WikipediaPage;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.Iterator;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -18,19 +14,10 @@ import java.util.stream.StreamSupport;
 public class WikipeadiaInput {
   public static Stream<WikipediaPage> dumpStreamFromResources(String dumpPath) {
     final ClassLoader classLoader = WikipeadiaInput.class.getClassLoader();
-    final URL fileUrl = classLoader.getResource(dumpPath);
-    if (fileUrl == null) {
-      throw new RuntimeException("Dump URL is null");
-    }
-    final File dumpFile = new File(fileUrl.getFile());
 
-    try {
-      final InputStream inputStream = new FileInputStream(dumpFile);
-      final Iterator<WikipediaPage> wikipediaPageIterator = new WikipediaPageIterator(inputStream);
-      final Iterable<WikipediaPage> iterable = () -> wikipediaPageIterator;
-      return StreamSupport.stream(iterable.spliterator(), false);
-    } catch (FileNotFoundException e) {
-      throw new RuntimeException(e);
-    }
+    final InputStream inputStream = classLoader.getResourceAsStream(dumpPath);
+    final Iterator<WikipediaPage> wikipediaPageIterator = new WikipediaPageIterator(inputStream);
+    final Iterable<WikipediaPage> iterable = () -> wikipediaPageIterator;
+    return StreamSupport.stream(iterable.spliterator(), false);
   }
 }
