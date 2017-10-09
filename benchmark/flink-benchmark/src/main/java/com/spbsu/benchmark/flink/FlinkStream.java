b2c8b98eb9ebe95bf781e64493a9278a8174b35f
Nikita Marshalkin <marnikitta@gmail.com>
2017-10-09 12:51:48 +0300

    Add suitable interface

--- benchmark/flink-benchmark/src/main/java/com/spbsu/benchmark/flink/FlinkStream.java
+++ benchmark/flink-benchmark/src/main/java/com/spbsu/benchmark/flink/FlinkStream.java
@@ -2,10 +2,17 @@ package com.spbsu.benchmark.flink;
 
 import org.apache.flink.streaming.api.datastream.DataStream;
 
+import java.util.function.Function;
+
 /**
  * User: Artem
  * Date: 05.10.2017
  */
-public interface FlinkStream<T, R> {
+public interface FlinkStream<T, R> extends Function<DataStream<T>, DataStream<R>> {
+  @Override
+  default DataStream<R> apply(DataStream<T> tDataStream) {
+    return stream(tDataStream);
+  }
+
   DataStream<R> stream(DataStream<T> source);
 }
