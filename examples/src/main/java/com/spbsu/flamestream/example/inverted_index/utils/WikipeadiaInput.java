12c502c46963cf6367a88ddaa13d64f4415f90f2
Nikita Marshalkin <marnikitta@gmail.com>
2017-10-10 17:53:26 +0300

    Wrap streams in bufferStream

--- examples/src/main/java/com/spbsu/flamestream/example/inverted_index/utils/WikipeadiaInput.java
+++ examples/src/main/java/com/spbsu/flamestream/example/inverted_index/utils/WikipeadiaInput.java
@@ -2,6 +2,7 @@ package com.spbsu.flamestream.example.inverted_index.utils;
 
 import com.spbsu.flamestream.example.inverted_index.model.WikipediaPage;
 
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -16,13 +17,13 @@ import java.util.stream.StreamSupport;
 public class WikipeadiaInput {
   public static Stream<WikipediaPage> dumpStreamFromResources(String dumpPath) {
     final ClassLoader classLoader = WikipeadiaInput.class.getClassLoader();
-    final InputStream inputStream = classLoader.getResourceAsStream(dumpPath);
+    final BufferedInputStream inputStream = new BufferedInputStream(classLoader.getResourceAsStream(dumpPath));
     return fromInputStream(inputStream);
   }
 
   public static Stream<WikipediaPage> dumpStreamFromFile(String filePath) {
     try {
-      final InputStream inputStream = new FileInputStream(filePath);
+      final BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(filePath));
       return fromInputStream(inputStream);
     } catch (FileNotFoundException e) {
       throw new RuntimeException(e);
