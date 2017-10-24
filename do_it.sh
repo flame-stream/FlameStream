bbe44436da5ff8fe821d428d8ffcb7c70873e1d9
Nikita Marshalkin <marnikitta@gmail.com>
2017-10-24 14:25:19 +0300

    Change inventory

--- do_it.sh
+++ do_it.sh
@@ -5,10 +5,9 @@ ANSIBLE_HOME=benchmark/ansible
 
 echo "Running benchmark-suite for ${1}"
 
-mvn clean 
-mvn -DskipTests=true package
+mvn -DskipTests=true clean package
   
 benchmark/ansible/copy_artifacts.sh 
 
-ansible-playbook -v -i ${ANSIBLE_HOME}/inventories/aws/hosts ${ANSIBLE_HOME}/${1}.yml -t destroy
-ansible-playbook -v -i ${ANSIBLE_HOME}/inventories/aws/hosts ${ANSIBLE_HOME}/${1}.yml -t deliver,bootstrap,run
+ansible-playbook -v -i ${ANSIBLE_HOME}/inventories/aws-bench/hosts ${ANSIBLE_HOME}/${1}.yml -t destroy
+ansible-playbook -v -i ${ANSIBLE_HOME}/inventories/aws-bench/hosts ${ANSIBLE_HOME}/${1}.yml -t deliver,bootstrap,run
