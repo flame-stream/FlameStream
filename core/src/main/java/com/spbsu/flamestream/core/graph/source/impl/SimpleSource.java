c8295911258a4a426d1f94928b7218a328cd0325
Artem Trofimov <trofimov9artem@gmail.com>
2017-11-14 16:14:45 +0300

    Adapt simple source

--- core/src/main/java/com/spbsu/flamestream/core/graph/source/impl/SimpleSource.java
+++ core/src/main/java/com/spbsu/flamestream/core/graph/source/impl/SimpleSource.java
@@ -13,6 +13,8 @@ public class SimpleSource extends AbstractSource {
   public void onNext(DataItem<?> item, SourceHandle handle) {
     handle.push(outPort, item);
     handle.ack(item.ack(), item.meta().globalTime());
+
     handle.accept(item.meta().globalTime());
+    super.onNext(item, handle);
   }
 }
