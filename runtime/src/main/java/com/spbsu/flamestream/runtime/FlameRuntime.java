202613dcf6d61261d52aa451cbfbe31d01b298d8
Nikita Marshalkin <marnikitta@gmail.com>
2017-11-28 13:11:55 +0300

    Remove obsolete

--- runtime/src/main/java/com/spbsu/flamestream/runtime/FlameRuntime.java
+++ runtime/src/main/java/com/spbsu/flamestream/runtime/FlameRuntime.java
@@ -4,7 +4,7 @@ import com.spbsu.flamestream.core.Front;
 import com.spbsu.flamestream.core.Graph;
 import com.spbsu.flamestream.core.Rear;
 
-import java.util.Properties;
+import java.util.List;
 import java.util.stream.Stream;
 
 public interface FlameRuntime {
@@ -13,8 +13,15 @@ public interface FlameRuntime {
   interface Flame {
     void extinguish();
 
-    <T extends Front<H>, H extends Front.Handle> Stream<H> attachFront(Class<T> front, Properties props);
+    <T extends Front, H extends FrontHandle> Stream<H> attachFront(Class<T> front, List<Object> props);
 
-    <T extends Rear<H>, H extends Rear.Handle> Stream<H> outputOf(Class<T> rear, Properties props);
+    <T extends Rear, H extends RearHandle> Stream<H> outputOf(Class<T> rear, List<Object> props);
+  }
+
+  interface FrontHandle {
+  }
+
+  interface RearHandle {
+
   }
 }
