c32cc532f2c1a96e5885a329315d7a38c6549dcd
Artem Trofimov <trofimov9artem@gmail.com>
2017-11-28 17:02:47 +0300

    commit

---
+++ runtime/src/main/java/com/spbsu/flamestream/runtime/node/graph/GraphRouter.java
@@ -0,0 +1,11 @@
+package com.spbsu.flamestream.runtime.node.graph;
+
+import akka.actor.ActorRef;
+import com.spbsu.flamestream.core.DataItem;
+import com.spbsu.flamestream.core.Graph;
+
+public interface GraphRouter {
+  void tell(DataItem<?> message, Graph.Vertex destanation, ActorRef sender);
+
+  void broadcast(Object message, ActorRef sender);
+}
--- runtime/src/main/java/com/spbsu/flamestream/runtime/node/graph/materialization/GraphMaterialization.java
+++ runtime/src/main/java/com/spbsu/flamestream/runtime/node/graph/materialization/GraphMaterialization.java
@@ -5,24 +5,26 @@ import akka.actor.Props;
 import akka.japi.pf.ReceiveBuilder;
 import com.spbsu.flamestream.core.DataItem;
 import com.spbsu.flamestream.core.Graph;
+import com.spbsu.flamestream.runtime.node.graph.GraphRouter;
 import com.spbsu.flamestream.runtime.node.graph.acker.api.MinTimeUpdate;
 import com.spbsu.flamestream.runtime.node.graph.materialization.api.AddressedItem;
 import com.spbsu.flamestream.runtime.node.graph.materialization.api.Commit;
 import com.spbsu.flamestream.runtime.node.graph.materialization.vertices.VertexMaterialization;
 import com.spbsu.flamestream.runtime.utils.akka.LoggingActor;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
 public class GraphMaterialization extends LoggingActor {
-  private final Map<String, Stream<VertexMaterialization>> materialization = new HashMap<>();
+  private final Map<String, VertexMaterialization> materialization = new HashMap<>();
 
-  private GraphMaterialization(Graph graph, ActorRef router, ActorRef acker, ActorRef barrier) {
+  private GraphMaterialization(Graph graph, GraphRouter router, ActorRef acker, ActorRef barrier) {
 
   }
 
-  public static Props props(Graph graph, ActorRef router, ActorRef acker, ActorRef barrier) {
+  public static Props props(Graph graph, GraphRouter router, ActorRef acker, ActorRef barrier) {
     return Props.create(GraphMaterialization.class, graph, router, acker, barrier);
   }
 
@@ -36,7 +38,7 @@ public class GraphMaterialization extends LoggingActor {
   }
 
   private void onAddressedItem(AddressedItem atomicMessage) {
-    final Stream<VertexMaterialization> nextVertices = materialization.get(atomicMessage.vertexId());
+    final VertexMaterialization nextVertices = materialization.get(atomicMessage.vertexId());
     nextVertices.forEach(materialization -> {
       final Stream<DataItem<?>> out = materialization.apply(atomicMessage.item());
       out.forEach(dataItem -> onAddressedItem(new AddressedItem(dataItem, materialization.vertexId())));
