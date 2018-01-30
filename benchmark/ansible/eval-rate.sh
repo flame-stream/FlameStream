21290650345ce61d31fce4a15ee9492cd5ae147d
Nikita Marshalkin <marnikitta@gmail.com>
2018-01-30 15:37:20 +0300

    Add util scripts

--- benchmark/ansible/eval-rate.sh
+++ benchmark/ansible/eval-rate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 run() {
-  for rate in 300 200 150 100 80 40 20 10; do
+  for rate in 300 250 200 150 125 100 90 80 70; do
     ansible-playbook -v -i aws.yml flamestream.yml --extra-vars "rate=$rate"
     echo "$rate done"
   done;
