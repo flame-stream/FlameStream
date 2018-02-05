4b86b25fd7677196e9d558c6b079c5e87729f55d
Nikita Marshalkin <marnikitta@gmail.com>
2018-02-05 12:52:54 +0300

    Add ack creation event

--- runtime/src/main/java/com/spbsu/flamestream/runtime/acker/api/Ack.java
+++ runtime/src/main/java/com/spbsu/flamestream/runtime/acker/api/Ack.java
@@ -1,14 +1,17 @@
 package com.spbsu.flamestream.runtime.acker.api;
 
 import com.spbsu.flamestream.core.data.meta.GlobalTime;
+import com.spbsu.flamestream.runtime.utils.tracing.Tracing;
 
 public class Ack {
   private final GlobalTime time;
   private final long xor;
+  private static final Tracing.Tracer tracer = Tracing.TRACING.forEvent("ack-create");
 
   public Ack(GlobalTime time, long xor) {
     this.time = time;
     this.xor = xor;
+    tracer.log(xor);
   }
 
   public GlobalTime time() {
