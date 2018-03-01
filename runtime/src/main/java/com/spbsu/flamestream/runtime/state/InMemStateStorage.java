7673c7dfb21271d72c249fb06afcab861dbd5093
Artem Trofimov <trofimov9artem@gmail.com>
2018-03-01 13:46:21 +0300

    adsad

--- runtime/src/main/java/com/spbsu/flamestream/runtime/state/InMemStateStorage.java
+++ runtime/src/main/java/com/spbsu/flamestream/runtime/state/InMemStateStorage.java
@@ -9,7 +9,7 @@ public class InMemStateStorage implements StateStorage {
   private final SortedMap<String, byte[]> storage = new TreeMap<>();
 
   @Override
-  public void keys(String opId, int from, int to, IntObjConsumer<byte[]> consumer) {
+  public void get(String opId, int from, int to, IntObjConsumer<byte[]> consumer) {
     storage.subMap(String.valueOf(from) + "_" + opId, String.valueOf(to) + opId).forEach((s, bytes) -> {
       consumer.accept(Integer.valueOf(s.split("_")[0]), bytes);
     });
--- runtime/src/main/java/com/spbsu/flamestream/runtime/state/StateStorage.java
+++ runtime/src/main/java/com/spbsu/flamestream/runtime/state/StateStorage.java
@@ -1,12 +1,9 @@
 package com.spbsu.flamestream.runtime.state;
 
-import com.expleague.commons.util.Pair;
 import org.agrona.collections.IntObjConsumer;
 
-import java.util.stream.Stream;
-
 public interface StateStorage {
-  void keys(String opId, int from, int to, IntObjConsumer<byte[]> consumer);
+  void get(String opId, int from, int to, IntObjConsumer<byte[]> consumer);
 
   void put(String opId, int hash, byte[] value);
 }
