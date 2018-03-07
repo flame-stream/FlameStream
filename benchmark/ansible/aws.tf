3fcf852064c9d4b288dbe462720386bfdb688dc9
Nikita Marshalkin <marnikitta@gmail.com>
2018-03-07 15:16:34 +0300

    Update tf script

--- benchmark/ansible/aws.tf
+++ benchmark/ansible/aws.tf
@@ -8,11 +8,11 @@ variable "region" {
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  region = "${var.region}"
 }
 
 resource "aws_key_pair" "nikita" {
-  key_name   = "nikita"
+  key_name = "nikita"
   public_key = "${var.key_pair}"
 }
 
@@ -25,13 +25,13 @@ resource "aws_internet_gateway" "workers" {
 }
 
 resource "aws_subnet" "workers" {
-  vpc_id     = "${aws_vpc.workers.id}"
+  vpc_id = "${aws_vpc.workers.id}"
   cidr_block = "10.0.0.0/24"
   map_public_ip_on_launch = true
 }
 
 resource "aws_route_table" "workers" {
-  vpc_id     = "${aws_vpc.workers.id}"
+  vpc_id = "${aws_vpc.workers.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -40,7 +40,7 @@ resource "aws_route_table" "workers" {
 }
 
 resource "aws_route_table_association" "public" {
-  subnet_id      = "${aws_subnet.workers.id}"
+  subnet_id = "${aws_subnet.workers.id}"
   route_table_id = "${aws_route_table.workers.id}"
 }
 
@@ -48,31 +48,35 @@ resource "aws_security_group" "workers" {
   vpc_id = "${aws_vpc.workers.id}"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["${aws_vpc.workers.cidr_block}"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "${aws_vpc.workers.cidr_block}"]
   }
 
   ingress {
-    from_port   = 8081
-    to_port     = 8081
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 8081
+    to_port = 8081
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
 
   egress {
-    protocol    = -1
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol = -1
+    from_port = 0
+    to_port = 0
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
 }
 
@@ -82,9 +86,14 @@ resource "aws_instance" "worker" {
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.nikita.key_name}"
   subnet_id = "${aws_subnet.workers.id}"
-  vpc_security_group_ids = ["${aws_security_group.workers.id}"]
+  vpc_security_group_ids = [
+    "${aws_security_group.workers.id}"]
   associate_public_ip_address = true
 }
 
-output "public_ips"   { value = "${join(",", aws_instance.worker.*.public_ip)}" }
-output "private_ips"  { value = "${join(",", aws_instance.worker.*.private_ip)}" }
+output "public_ips" {
+  value = "${join(", ", aws_instance.worker.*.public_ip)}"
+}
+output "private_ips" {
+  value = "${join(", ", aws_instance.worker.*.private_ip)}"
+}
