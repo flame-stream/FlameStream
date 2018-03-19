7a7b59f06046f17ae092981f325a1bdb3e31f1a0
Artem Trofimov <trofimov9artem@gmail.com>
2018-03-19 16:44:19 +0300

    Increase timeouts

--- runtime/src/main/java/com/spbsu/flamestream/runtime/utils/FlameConfig.java
+++ runtime/src/main/java/com/spbsu/flamestream/runtime/utils/FlameConfig.java
@@ -6,8 +6,8 @@ import java.util.concurrent.TimeUnit;
 
 public class FlameConfig {
   public static final FlameConfig config = new FlameConfig(
-          Timeout.apply(2, TimeUnit.SECONDS),
-          Timeout.apply(10, TimeUnit.SECONDS)
+          Timeout.apply(5, TimeUnit.SECONDS),
+          Timeout.apply(20, TimeUnit.SECONDS)
   );
   private final Timeout smallTimeout;
   private final Timeout bigTimeout;
