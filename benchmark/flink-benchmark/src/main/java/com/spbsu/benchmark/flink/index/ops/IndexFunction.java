c426cf2d49aad550ee09019d0e5c9415c2203dcc
Nikita Marshalkin <marnikitta@gmail.com>
2018-03-22 14:28:18 +0300

    Make Flink's state asynchronous

--- benchmark/flink-benchmark/src/main/java/com/spbsu/benchmark/flink/index/ops/IndexFunction.java
+++ benchmark/flink-benchmark/src/main/java/com/spbsu/benchmark/flink/index/ops/IndexFunction.java
@@ -9,40 +9,23 @@ import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.typeutils.GenericTypeInfo;
-import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.util.Collector;
-
-import java.io.IOException;
 
 /**
  * User: Artem
  * Date: 04.01.2018
  */
-public class IndexFunction extends ProcessFunction<Tuple2<String, long[]>, Result> {
-  private transient ValueStateDescriptor<InvertedIndexState> descriptor;
+public class IndexFunction extends ProcessFunction<Tuple2<String, long[]>, Result> implements CheckpointedFunction {
+  private transient ValueState<InvertedIndexState> checkpointedState;
+  private transient InvertedIndexState index;
 
   @Override
-  public void open(Configuration parameters) throws Exception {
-    super.open(parameters);
-    descriptor = new ValueStateDescriptor<>(
-            "index_state",
-            new GenericTypeInfo<>(InvertedIndexState.class)
-    );
-  }
-
-  @Override
-  public void processElement(Tuple2<String, long[]> value, Context ctx, Collector<Result> out) throws IOException {
-    final ValueState<InvertedIndexState> state = getRuntimeContext().getState(descriptor);
-    final InvertedIndexState indexState;
-    if (state.value() == null) {
-      indexState = new InvertedIndexState();
-    } else {
-      indexState = state.value().copy();
-    }
-
-    final long prevValue = indexState.updateOrInsert(value.f1);
-    state.update(indexState);
+  public void processElement(Tuple2<String, long[]> value, Context ctx, Collector<Result> out) {
+    final long prevValue = index.updateOrInsert(value.f1);
 
     final WordIndexAdd wordIndexAdd = new WordIndexAdd(value.f0, value.f1);
     WordIndexRemove wordIndexRemove = null;
@@ -54,5 +37,21 @@ public class IndexFunction extends ProcessFunction<Tuple2<String, long[]>, Resul
       );
     }
     out.collect(new Result(wordIndexAdd, wordIndexRemove));
+  }
+
+  @Override
+  public void snapshotState(FunctionSnapshotContext context) throws Exception {
+    checkpointedState.clear();
+    checkpointedState.update(index);
+  }
+
+  @Override
+  public void initializeState(FunctionInitializationContext context) {
+    this.checkpointedState = context.getKeyedStateStore().getState(new ValueStateDescriptor<>(
+            "index_state",
+            new GenericTypeInfo<>(InvertedIndexState.class)
+    ));
+
+    index = new InvertedIndexState();
   }
 }
