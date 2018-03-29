abae4e9ccf993a3ed054fe2ee8cc7191fdb6f23e
Nikita Marshalkin <marnikitta@gmail.com>
2018-03-29 15:46:58 +0300

    Increase _bigTimeout_ so there is a time for processes to restart

--- runtime/src/main/java/com/spbsu/flamestream/runtime/utils/FlameConfig.java
+++ runtime/src/main/java/com/spbsu/flamestream/runtime/utils/FlameConfig.java
@@ -7,7 +7,7 @@ import java.util.concurrent.TimeUnit;
 public class FlameConfig {
   public static final FlameConfig config = new FlameConfig(
           Timeout.apply(1, TimeUnit.SECONDS),
-          Timeout.apply(20, TimeUnit.SECONDS)
+          Timeout.apply(50, TimeUnit.SECONDS)
   );
   private final Timeout smallTimeout;
   private final Timeout bigTimeout;
