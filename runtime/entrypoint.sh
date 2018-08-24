229c034cc048ceffb4b34f22c3f9539a5c0a5a1b
Artem Trofimov <trofimov9artem@gmail.com>
2018-08-25 00:54:39 +0300

    Adjust artery/aeron params

--- runtime/entrypoint.sh
+++ runtime/entrypoint.sh
@@ -7,7 +7,7 @@ usage() {
 start() {
   local config=$1
   echo "Starting flamestream worker, config path: $config"
-  local java_ops="-Daeron.term.buffer.length=4194304 -Xms500m -Xmx500m -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+HeapDumpOnOutOfMemoryError"
+  local java_ops="-Daeron.term.buffer.length=4194304 -Daeron.mtu.length=16384 -Xms500m -Xmx500m -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+HeapDumpOnOutOfMemoryError"
   echo "java_ops=$java_ops"
   local main="com.spbsu.flamestream.runtime.application.WorkerApplication"
   local command="while :; do java $java_ops -cp lib/*:flamestream-runtime-1.0-SNAPSHOT.jar $main $config > worker.log 2>&1; done"
