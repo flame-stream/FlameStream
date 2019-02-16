051379a187507fce02f3b8899f0524a3441cfbdf
Sergey Reznick <sergey.reznick@gmail.com>
2019-02-16 04:04:27 +0300

    TF-TDF graph with test on lenta dataset

---
+++ examples/src/main/java/com/spbsu/flamestream/example/bl/tfidf/ops/filtering/DocContainerOrderingFilter.java
@@ -0,0 +1,35 @@
+package com.spbsu.flamestream.example.bl.tfidf.ops.filtering;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import com.spbsu.flamestream.example.bl.tfidf.model.IDFObject;
+import com.spbsu.flamestream.example.bl.tfidf.model.containers.DocContainer;
+import com.spbsu.flamestream.example.bl.tfidf.model.counters.WordCounter;
+
+public class DocContainerOrderingFilter implements Function<List<DocContainer>, Stream<List<DocContainer>>> {
+
+  @Override
+  public Stream<List<DocContainer>> apply(List<DocContainer> docContainers) {
+    if (docContainers.size() > 2) {
+      throw new IllegalStateException("Group size should be <= 2");
+    }
+
+    if (docContainers.size() == 1 && !(docContainers.get(0) instanceof WordCounter)) {
+      throw new IllegalStateException(String.format("The only element in group should be WordCounter: %s (%s)",
+              docContainers.get(0), docContainers.get(0).getClass()));
+    }
+
+    if (docContainers.size() == 1 || (docContainers.get(0) instanceof IDFObject
+            && docContainers.get(1) instanceof WordCounter)) {
+      if (docContainers.size() == 2 && !docContainers.get(0).document().equals(docContainers.get(1).document())) {
+        return Stream.of(Collections.singletonList(docContainers.get(1)));
+      }
+      return Stream.of(docContainers);
+    } else {
+      return Stream.empty();
+    }
+  }
+}
---
+++ examples/src/main/java/com/spbsu/flamestream/example/bl/tfidf/ops/filtering/WordContainerOrderingFilter.java
@@ -0,0 +1,31 @@
+package com.spbsu.flamestream.example.bl.tfidf.ops.filtering;
+
+import com.spbsu.flamestream.example.bl.tfidf.model.containers.WordContainer;
+import com.spbsu.flamestream.example.bl.tfidf.model.counters.WordCounter;
+import com.spbsu.flamestream.example.bl.tfidf.model.entries.WordEntry;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class WordContainerOrderingFilter implements Function<List<WordContainer>, Stream<List<WordContainer>>> {
+
+  @Override
+  public Stream<List<WordContainer>> apply(List<WordContainer> wordContainers) {
+    if (wordContainers.size() > 2) {
+      throw new IllegalStateException("Group size should be <= 2");
+    }
+
+    if (wordContainers.size() == 1 && !(wordContainers.get(0) instanceof WordEntry)) {
+      throw new IllegalStateException(String.format("The only element in group should be WordEntry: %s (%s)",
+              wordContainers.get(0), wordContainers.get(0).getClass()));
+    }
+
+    if (wordContainers.size() == 1 || (wordContainers.get(0) instanceof WordCounter
+            && wordContainers.get(1) instanceof WordEntry)) {
+      return Stream.of(wordContainers);
+    } else {
+      return Stream.empty();
+    }
+  }
+}
