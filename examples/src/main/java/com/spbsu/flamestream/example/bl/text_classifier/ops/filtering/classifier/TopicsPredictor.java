7e7e8f543ee30562b60dfdca34061dce8a6668bc
shavkunov <mv.shavkunov@yandex.ru>
2019-02-18 18:20:46 +0300

    computing gradient

--- examples/src/main/java/com/spbsu/flamestream/example/bl/text_classifier/ops/filtering/classifier/TopicsPredictor.java
+++ examples/src/main/java/com/spbsu/flamestream/example/bl/text_classifier/ops/filtering/classifier/TopicsPredictor.java
@@ -1,8 +1,12 @@
 package com.spbsu.flamestream.example.bl.text_classifier.ops.filtering.classifier;
 
+import com.expleague.commons.math.vectors.Mx;
+
 public interface TopicsPredictor {
   default void init() {
   }
 
   Topic[] predict(Document document);
+
+  void updateWeights(Mx trainingSet, String[] correctTopics);
 }
