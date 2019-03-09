93713d47a402eaa2d4fdd2a96f80103909737fdd
Artem Trofimov <trofimov9artem@gmail.com>
2019-03-10 02:48:06 +0300

    Hack for avoiding grouping replay
    Former-commit-id: 72916a791680cf0803c25f4b448170e1f7f51435

--- examples/src/main/java/com/spbsu/flamestream/example/bl/text_classifier/model/IdfObject.java
+++ examples/src/main/java/com/spbsu/flamestream/example/bl/text_classifier/model/IdfObject.java
@@ -3,31 +3,18 @@ package com.spbsu.flamestream.example.bl.text_classifier.model;
 import com.spbsu.flamestream.example.bl.text_classifier.model.containers.DocContainer;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class IdfObject implements DocContainer {
   private final Map<String, Integer> counts = new HashMap<>();
   private final String docName;
   private final String partitioning;
-  private final int idfCardinality;
 
-  public boolean isComplete() {
-    return counts.size() == idfCardinality;
-  }
-
-  public IdfObject(WordCounter wordCounter) {
-    this.docName = wordCounter.document();
-    this.partitioning = wordCounter.partitioning();
-    counts.put(wordCounter.word(), wordCounter.count());
-    this.idfCardinality = wordCounter.idfCardinality();
-  }
-
-  public IdfObject(IdfObject some, WordCounter wordCounter) {
-    this.docName = some.docName;
-    this.partitioning = some.partitioning;
-    this.idfCardinality = some.idfCardinality;
-    counts.putAll(some.counts);
-    counts.put(wordCounter.word(), wordCounter.count());
+  public IdfObject(List<WordCounter> counters) {
+    counters.forEach(wordCounter -> counts.put(wordCounter.word(), wordCounter.count()));
+    docName = counters.get(0).document();
+    partitioning = counters.get(0).partitioning();
   }
 
   @Override
