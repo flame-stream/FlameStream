7bd448f0f07d6e5a4db64b3b882a8b866575305b
Artem Trofimov <trofimov9artem@gmail.com>
2019-05-13 21:16:08 +0300

    Small refactoring + disable python test

--- examples/src/main/java/com/spbsu/flamestream/example/bl/text_classifier/ops/filtering/classifier/ModelState.java
+++ examples/src/main/java/com/spbsu/flamestream/example/bl/text_classifier/ops/filtering/classifier/ModelState.java
@@ -1,28 +1,7 @@
 package com.spbsu.flamestream.example.bl.text_classifier.ops.filtering.classifier;
 
 import com.expleague.commons.math.vectors.Mx;
-import com.expleague.commons.math.vectors.impl.mx.VecBasedMx;
 
-public class ModelState {
-  private final Mx weights;
-  private final Mx zed;
-  private final Mx norm;
-
-  public Mx weights() {
-    return weights;
-  }
-
-  public Mx zed() {
-    return zed;
-  }
-
-  public Mx norm() {
-    return norm;
-  }
-
-  public ModelState(Mx prevWeights) {
-    weights = new VecBasedMx(prevWeights.rows(), prevWeights.columns());
-    zed = new VecBasedMx(prevWeights.rows(), prevWeights.columns());
-    norm = new VecBasedMx(prevWeights.rows(), prevWeights.columns());
-  }
+public interface ModelState {
+  Mx weights();
 }
