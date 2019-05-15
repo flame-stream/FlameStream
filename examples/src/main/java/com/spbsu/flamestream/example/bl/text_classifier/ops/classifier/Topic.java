0aac392d24c11fc706c9bec2a168263187575cbc
Artem Trofimov <trofimov9artem@gmail.com>
2019-05-15 21:57:30 +0300

    Add online predictor

--- examples/src/main/java/com/spbsu/flamestream/example/bl/text_classifier/ops/classifier/Topic.java
+++ examples/src/main/java/com/spbsu/flamestream/example/bl/text_classifier/ops/classifier/Topic.java
@@ -7,7 +7,7 @@ public class Topic implements Comparable<Topic> {
   private final String id;
   private final double probability;
 
-  Topic(String name, String id, double probability) {
+  public Topic(String name, String id, double probability) {
     this.name = name;
     this.id = id;
     this.probability = probability;
