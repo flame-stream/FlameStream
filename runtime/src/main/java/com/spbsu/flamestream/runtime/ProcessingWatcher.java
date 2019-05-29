b81fd665fd94827b54605c9b24e396098da1b97e
Nikita Sokolov <faucct@gmail.com>
2019-05-29 13:35:58 +0300

    LocalAcker should not abstract ackers from RegistryHolder

--- runtime/src/main/java/com/spbsu/flamestream/runtime/ProcessingWatcher.java
+++ runtime/src/main/java/com/spbsu/flamestream/runtime/ProcessingWatcher.java
@@ -168,7 +168,7 @@ public class ProcessingWatcher extends LoggingActor {
     final ActorRef localAcker = context().actorOf(LocalAcker.props(ackers));
     final ActorRef committer, registryHolder;
     if (zookeeperWorkersNode.isLeader(id)) {
-      registryHolder = context().actorOf(RegistryHolder.props(new ZkRegistry(curator), localAcker), "registry-holder");
+      registryHolder = context().actorOf(RegistryHolder.props(new ZkRegistry(curator), ackers), "registry-holder");
       committer = context().actorOf(Committer.props(
               config.paths().size(),
               systemConfig,
