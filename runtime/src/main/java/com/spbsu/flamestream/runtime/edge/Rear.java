c7e498810ab0caee327c113d7019d0e818b8b376
Nikita Sokolov <faucct@gmail.com>
2019-07-09 18:56:51 +0300

    SocketRear should emit MinTime messages if barrier is disabled
    Former-commit-id: ca73aaf7df57230af28a3c703310c1918194e60a

--- runtime/src/main/java/com/spbsu/flamestream/runtime/edge/Rear.java
+++ runtime/src/main/java/com/spbsu/flamestream/runtime/edge/Rear.java
@@ -1,12 +1,19 @@
 package com.spbsu.flamestream.runtime.edge;
 
 import com.spbsu.flamestream.core.Batch;
+import com.spbsu.flamestream.core.data.meta.GlobalTime;
 
 /**
  * User: Artem
  * Date: 27.11.2017
  */
 public interface Rear {
+  class MinTime {
+    public final GlobalTime time;
+
+    public MinTime(GlobalTime time) {this.time = time;}
+  }
+
   /**
    * Sync call, return mean accept
    */
