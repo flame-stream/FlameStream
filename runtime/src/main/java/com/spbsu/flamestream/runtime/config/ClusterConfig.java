317e5249ca3345c7a4be11742fe392652491eb42
faucct <faucct@gmail.com>
2019-09-17 08:44:23 +0300

    Extract hashGroups() from ClusterConfig (#238)

--- runtime/src/main/java/com/spbsu/flamestream/runtime/config/ClusterConfig.java
+++ runtime/src/main/java/com/spbsu/flamestream/runtime/config/ClusterConfig.java
@@ -2,8 +2,6 @@ package com.spbsu.flamestream.runtime.config;
 
 import akka.actor.ActorPath;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,12 +10,10 @@ import java.util.stream.Collectors;
 public class ClusterConfig {
   private final Map<String, ActorPath> paths;
   private final String masterLocation;
-  private final Map<String, HashGroup> hashGroups;
 
-  public ClusterConfig(Map<String, ActorPath> paths, String masterLocation, Map<String, HashGroup> hashGroups) {
+  public ClusterConfig(Map<String, ActorPath> paths, String masterLocation) {
     this.paths = paths;
     this.masterLocation = masterLocation;
-    this.hashGroups = hashGroups;
   }
 
   public Map<String, ActorPath> paths() {
@@ -32,14 +28,10 @@ public class ClusterConfig {
     return paths.get(masterLocation);
   }
 
-  public Map<String, HashGroup> hashGroups() {
-    return new HashMap<>(hashGroups);
-  }
-
   public ClusterConfig withChildPath(String childPath) {
     final Map<String, ActorPath> newPaths = new HashMap<>();
     paths.forEach((s, path) -> newPaths.put(s, path.child(childPath)));
-    return new ClusterConfig(newPaths, masterLocation, hashGroups);
+    return new ClusterConfig(newPaths, masterLocation);
   }
 
   @Override
@@ -47,27 +39,14 @@ public class ClusterConfig {
     return "ClusterConfig{" +
             "paths=" + paths +
             ", masterLocation='" + masterLocation + '\'' +
-            ", hashGroups=" + hashGroups +
             '}';
   }
 
   public static ClusterConfig fromWorkers(List<ZookeeperWorkersNode.Worker> workers) {
     final Map<String, ActorPath> paths = workers.stream()
             .collect(Collectors.toMap(ZookeeperWorkersNode.Worker::id, ZookeeperWorkersNode.Worker::actorPath));
-    final Map<String, HashGroup> ranges = new HashMap<>();
-    final List<HashUnit> covering = HashUnit.covering(paths.size() - 1)
-            .collect(Collectors.toCollection(ArrayList::new));
     final String masterLocation = workers.get(0).id;
-    paths.keySet().forEach(s -> {
-      if (s.equals(masterLocation)) {
-        ranges.put(s, new HashGroup(Collections.singleton(new HashUnit(0, 0))));
-      } else {
-        ranges.put(s, new HashGroup(Collections.singleton(covering.get(0))));
-        covering.remove(0);
-      }
-    });
-    assert covering.isEmpty();
 
-    return new ClusterConfig(paths, masterLocation, ranges);
+    return new ClusterConfig(paths, masterLocation);
   }
 }
