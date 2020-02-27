b6a74304c41d7966544e8abe037ce30f688c5fd5
Nikita Sokolov <faucct@gmail.com>
2020-02-27 08:22:19 +0300

    GroupingState cleanup

--- runtime/src/main/java/com/spbsu/flamestream/runtime/graph/state/GroupGroupingState.java
+++ runtime/src/main/java/com/spbsu/flamestream/runtime/graph/state/GroupGroupingState.java
@@ -1,9 +1,9 @@
 package com.spbsu.flamestream.runtime.graph.state;
 
 import com.spbsu.flamestream.core.DataItem;
-import com.spbsu.flamestream.core.Equalz;
 import com.spbsu.flamestream.core.HashFunction;
 import com.spbsu.flamestream.core.data.invalidation.InvalidatingBucket;
+import com.spbsu.flamestream.core.graph.Grouping;
 import com.spbsu.flamestream.runtime.config.HashUnit;
 import com.spbsu.flamestream.runtime.utils.collections.HashUnitMap;
 import com.spbsu.flamestream.runtime.utils.collections.ListHashUnitMap;
@@ -11,22 +11,23 @@ import com.spbsu.flamestream.runtime.utils.collections.ListHashUnitMap;
 import java.util.Collection;
 
 public class GroupGroupingState {
+  private final Grouping<?> grouping;
   private final HashUnitMap<GroupingState> unitStates = new ListHashUnitMap<>();
 
-  public GroupGroupingState() {
+  public GroupGroupingState(Grouping<?> grouping) {
+    this.grouping = grouping;
   }
 
-  public GroupGroupingState(Collection<HashUnit> units) {
-    units.forEach(u -> unitStates.put(u, new GroupingState()));
+  public GroupGroupingState(Grouping<?> grouping, Collection<HashUnit> units) {
+    this.grouping = grouping;
+    units.forEach(u -> unitStates.put(u, new GroupingState(grouping)));
   }
 
   public void addUnitState(HashUnit unit, GroupingState unitState) {
     unitStates.put(unit, unitState);
   }
 
-  public InvalidatingBucket bucketFor(DataItem item, HashFunction hashFunction, Equalz equalz) {
-    final int hash = hashFunction.hash(item);
-    final GroupingState state = unitStates.get(hash);
-    return state.bucketFor(item, hashFunction, equalz);
+  public InvalidatingBucket bucketFor(DataItem item) {
+    return unitStates.get(grouping.hash().hash(item)).bucketFor(item);
   }
 }
