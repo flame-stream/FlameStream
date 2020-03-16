1c52bb895b4e1c55cfb4e2d44870cbd07ebac2eb
Nikita Sokolov <faucct@gmail.com>
2020-03-16 19:21:00 +0300

    Forward SIGINT to bench
    Former-commit-id: f157dde0797e7ca65d4bd9b6245de38c269c0da1

--- benchmark/ansible/run_sigmod_benchmarks_aws.py
+++ benchmark/ansible/run_sigmod_benchmarks_aws.py
@@ -26,7 +26,7 @@ def run_benchmarks(rate=2., iterations=100, results_name="", bench_environment={
     )
     flamestream_dir = "/home/ubuntu/flamestream"
     os.system(
-      "ssh flamestream-benchmarks-manager 'PATH=/usr/lib/jvm/java-8-oracle/jre/bin/:$PATH' " \
+      "ssh -t flamestream-benchmarks-manager 'PATH=/usr/lib/jvm/java-8-oracle/jre/bin/:$PATH' " \
       'EDGES_TAIL_FILE=/home/ubuntu/tail_head_offset.bin EDGES_HEAD_FILE=/home/ubuntu/edge_head.bin ' \
       "java -Daeron.term.buffer.length=4194304 -Daeron.mtu.length=16384 " \
       "-Xms500m -Xmx500m -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps " \
