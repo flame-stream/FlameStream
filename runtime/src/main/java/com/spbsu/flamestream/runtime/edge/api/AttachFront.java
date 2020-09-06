01ce78fe5b203f6c71ee0952aa8866139cd20c72
Nikita Sokolov <faucct@gmail.com>
2020-09-06 12:14:07 +0300

    Snapshots
    Former-commit-id: 73b76612177d11976521da68540f8106dc9ad4a8

--- runtime/src/main/java/com/spbsu/flamestream/runtime/edge/api/AttachFront.java
+++ runtime/src/main/java/com/spbsu/flamestream/runtime/edge/api/AttachFront.java
@@ -6,10 +6,12 @@ import com.spbsu.flamestream.runtime.FlameRuntime;
 public class AttachFront<F extends Front> {
   private final String id;
   private final FlameRuntime.FrontInstance<F> instance;
+  public final int trackingWindow;
 
-  public AttachFront(String id, FlameRuntime.FrontInstance<F> instance) {
+  public AttachFront(String id, FlameRuntime.FrontInstance<F> instance, int trackingWindow) {
     this.id = id;
     this.instance = instance;
+    this.trackingWindow = trackingWindow;
   }
 
   public String id() {
