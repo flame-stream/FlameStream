c5dd73a9e4a308fa4a5541d68ecacdbf435bbbbb
Nikita Sokolov <faucct@gmail.com>
2020-09-09 22:44:44 +0300

    Fix tracking components acking
    Former-commit-id: fac007b2640a836a8853b6a0bd21b4123b955044

--- core/src/main/java/com/spbsu/flamestream/core/TrackingComponent.java
+++ core/src/main/java/com/spbsu/flamestream/core/TrackingComponent.java
@@ -4,24 +4,27 @@ package com.spbsu.flamestream.core;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class TrackingComponent implements Comparable<TrackingComponent> {
-  public static final TrackingComponent SOURCE = new TrackingComponent(0, Collections.emptyList());
-  public static final TrackingComponent MAX = new TrackingComponent(Integer.MAX_VALUE, Collections.emptyList());
+  public static final TrackingComponent DEFAULT = new TrackingComponent(0, Collections.emptySet());
 
   public final int index;
-  public final List<TrackingComponent> adjacent;
+  public final Set<TrackingComponent> inbound, outbound = new HashSet<>();
 
-  public TrackingComponent(int index, List<TrackingComponent> adjacent) {
+  public TrackingComponent(int index, Set<TrackingComponent> inbound) {
     if (index < 0)
       throw new IllegalArgumentException(String.valueOf(index));
-    for (final TrackingComponent trackingComponent : adjacent) {
+    for (final TrackingComponent trackingComponent : inbound) {
       if (index <= trackingComponent.index) {
         throw new IllegalArgumentException(trackingComponent.toString());
       }
     }
-    this.adjacent = Collections.unmodifiableList(adjacent);
+    for (final TrackingComponent trackingComponent : inbound) {
+      trackingComponent.outbound.add(this);
+    }
+    this.inbound = new HashSet<>(inbound);
     this.index = index;
   }
 
