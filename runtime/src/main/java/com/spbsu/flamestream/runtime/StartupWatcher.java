4c835008b3ce46fd762f46ae7bfee4a4484d6ea0
Nikita Sokolov <faucct@gmail.com>
2020-11-01 17:11:18 +0300

    Update akka and aeron

--- runtime/src/main/java/com/spbsu/flamestream/runtime/StartupWatcher.java
+++ runtime/src/main/java/com/spbsu/flamestream/runtime/StartupWatcher.java
@@ -86,12 +86,14 @@ public class StartupWatcher extends LoggingActor {
             ),
             "processing-watcher"
     );
+    final File file = new File("/tmp/flame_stream");
     //noinspection ResultOfMethodCallIgnored
-    new File("/tmp/flame_stream").createNewFile();
+    file.createNewFile();
+    file.deleteOnExit();
   }
 
   @Override
-  public void postStop() {
+  public void postStop() throws Exception {
     //noinspection EmptyTryBlock,unused
     try (
             StateStorage s = stateStorage;
