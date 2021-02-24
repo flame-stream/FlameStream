d2f05a35fc66b275698768fccf9ca3ea45eb91d2
Nikita Sokolov <faucct@yandex-team.ru>
2021-02-24 13:48:25 +0300

    Fact names should contain no dots
    Former-commit-id: 37036472e081512cb79f7f2e961aa2cde70d090b

--- benchmark/modules/my/lib/facter/etc-ssh-ssh_host_ecdsa_key.pub.rb
+++ benchmark/modules/my/lib/facter/etc-ssh-ssh_host_ecdsa_key.pub.rb
@@ -1,4 +1,4 @@
-Facter.add('/etc/ssh/ssh_host_ecdsa_key.pub') do
+Facter.add('/etc/ssh/ssh_host_ecdsa_key_pub') do
   setcode do
     file = '/etc/ssh/ssh_host_ecdsa_key.pub'
     Facter::Util::Resolution.exec("[ -r #{file} ] && cat #{file}")
